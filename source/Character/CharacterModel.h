a2c7ed3dddb4baa8bec09ea2dc911104fb6d87e4
qiuyichen00 <qiuyichen00@outlook.com>
2023-02-25 17:12:36 -0500

    edited the config-file to fix build issue

--- source/Character/CharacterModel.h
+++ source/Character/CharacterModel.h
@@ -8,5 +8,23 @@
 #ifndef CharacterModel_h
 #define CharacterModel_h
 
+#include <cugl/cugl.h>
+using namespace cugl;
 
+class CharacterModel{
+private:
+    /** Center of the character */
+    Vec2 _position;
+
+public:
+    /** Constructor */
+    CharacterModel(){
+        
+    }
+    
+    void setPosition(Vec2 posi){
+        _position = posi;
+    }
+    
+};
 #endif /* CharacterModel_h */
