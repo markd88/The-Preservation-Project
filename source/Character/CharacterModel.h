834a6a7108e56ed3104cb155aeff47900038bb59
Yun Zhou <yzhou776@gatech.edu>
2023-02-25 20:37:14 -0500

    finished character module

--- source/Character/CharacterModel.h
+++ source/Character/CharacterModel.h
@@ -1,6 +1,5 @@
 //
 //  CharacterModel.h
-//  Tilemap
 //
 //  Created by Yichen Qiu on 2/23/23.
 //
@@ -15,15 +14,55 @@ class CharacterModel{
 private:
     /** Center of the character */
     Vec2 _position;
-
+    Size _size;
+    Color4 _color;
+    
 public:
-    /** Constructor */
-    CharacterModel(){
-        
+    /** A public accessible, read-only version of the color */
+    const Color4& color;
+
+#pragma mark Main Functions
+public:
+    /**
+     * Creates the model state.
+     *
+     * @param position  The bottom left corner of the tile
+     * @param size      The width and height of a tile
+     * @param color     The tile color
+     */
+    CharacterModel(Vec2 position, Size size, Color4 color): color(_color) {
+        setPosition(position);
+        setSize(size);
+        setColor(color);
+    }
+
+#pragma mark Setters
+public:
+    /**
+     *  Sets the position of the bottom left corner of the tile.
+     *
+     *  @param position Bottom left corner of tile
+     */
+    void setPosition(Vec2 position) {
+        _position = position;
     }
     
-    void setPosition(Vec2 posi){
-        _position = posi;
+    /**
+     *  Sets the size of the tile.
+     *
+     *  @param size Width and height of a single tile
+     */
+    void setSize(Size size) {
+        _size = size;
+    }
+    
+    /**
+     *  Sets the color of the tile.
+     *
+     *  @param color The color of the tile
+     */
+    void setColor(Color4 color) {
+        _color = color;
     }
     
 };
--- source/Character/CharacterView.h
+++ source/Character/CharacterView.h
@@ -16,21 +16,61 @@ private:
     /** The node is attached to the root-scene*/
     std::shared_ptr<scene2::PolygonNode> _node;
     
+#pragma mark Main Functions
 public:
     /** contructor */
-    CharacterView(){
-        
+    CharacterView(Vec2 position, Size size, Color4 color){
+        // TODO: Implement me
+        // initialize view
+        _node = scene2::PolygonNode::alloc();
+        _node->setRelativeColor(false);
+        _node->setAnchor(Vec2::ANCHOR_BOTTOM_LEFT);
+        // initialize state
+        setSize(size);
+        setPosition(position);
+        setColor(color);
     }
-    
     
     ~CharacterView(){
-        
+        auto parent = _node->getParent();
+        if (parent != nullptr && _node != nullptr) {
+            parent->removeChild(_node);
+        }
     }
     
+#pragma mark Scene Methods
+public:
+    /**
+     * Adds the view components as children to the given `sceneNode`.
+     *
+     * @param sceneNode The scenenode to add the view to
+     */
+    void addChildTo(const std::shared_ptr<scene2::SceneNode>& sceneNode) {
+        sceneNode->addChild(_node);
+    }
+    
+    /**
+     * Removes the view component children from the given `sceneNode`.
+     *
+     * @param sceneNode The scenenode to remove the view from
+     */
+    void removeChildFrom(const std::shared_ptr<scene2::SceneNode>& sceneNode) {
+        sceneNode->removeChild(_node);
+    }
+
+#pragma mark Setters
+public:
     void setPosition(Vec2 position){
-        
+        _node->setPosition(position);
     }
     
+    void setSize(Size size){
+        _node->setPolygon(Rect(Vec2(0,0), size));
+    }
+    
+    void setColor(Color4 color){
+        _node->setColor(color);
+    }
 };
 
 
