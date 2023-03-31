0b6dd1e5eccb8bfea087fcd7d135cfa683f98cac
Hao Chen <haochen3556@gmail.com>
2023-03-31 17:20:19 -0400

    minor param edit

--- source/Level/LevelModel.cpp
+++ source/Level/LevelModel.cpp
@@ -21,7 +21,6 @@ LevelModel::LevelModel(void) : Asset()
 {
     _world = std::make_unique<TilemapController>();
     _item = std::make_shared<ArtifactSetController>();
-//    _resource = std::make_shared<ArtifactSetController>();
 }
 
 /**
@@ -133,10 +132,10 @@ bool LevelModel::loadTilemap(const std::shared_ptr<JsonValue>& json) {
     int height = json->get("width")->asInt();
     int x = json->get("x")->asInt() / width;
     int y = json->get("y")->asInt() / height - 1;
-//
-//    x = 896 - x;
-//    y = 1536 - y;
-//
+    
+//    x = 1536 -x;
+//    y = 896 -y;
+    
     // TODO: replace below
     _world->addTile2(x, y, false, _assets, textureType);
     
@@ -176,9 +175,9 @@ bool LevelModel::loadArtifact(const std::shared_ptr<JsonValue>& json) {
     int width = json->get("height")->asInt() /2;
     int height = json->get("width")->asInt() /2;
 //    int x = json->get("x")->asInt() -738;
-//    int y = 1536 - json->get("y")->asInt() -430.5 -500;
+//    int y = 896 - json->get("y")->asInt() -430.5;
     int x = json->get("x")->asInt();
-    int y = 1536 - json->get("y")->asInt() -600;
+    int y = 896 - json->get("y")->asInt() +40; 
     
     Vec2 pos = Vec2 (x, y);
     Size size = Size(width, height);
@@ -195,5 +194,4 @@ bool LevelModel::loadArtifact(const std::shared_ptr<JsonValue>& json) {
 void LevelModel::setTilemapTexture() {
     _world->setTexture(_assets);
     _item->setTexture(_assets);
-//    _resource->setTexture(_assets);
 };
